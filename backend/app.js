b2fbf6b9cad7e8c45bbfc8495380532eab859c9b
AnjaliRaj05 <92683584+AnjaliRaj05@users.noreply.github.com>
2023-04-28 11:43:42 +0530

    Todo_appilcation

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+const { route: userRoute } = require('./Routes/UserRoute')
+const { route: taskRoute } = require('./Routes/TaskRoute')
+const { error: errorMW } = require('./middlewares/error')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  next()
+})
+
+app.use(cors())
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use('/api/v1', userRoute)
+app.use('/api/v1', taskRoute)
+
+app.use(errorMW)
+
+module.exports.app = app;
